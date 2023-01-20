3f13f7c6c6edfe86c7f36c7620283dd45e8744e3
Alessandro <64756373+AlessBD99@users.noreply.github.com>
2023-01-20 19:55:48 -0400

    Primera Pregunta

--- src/Parcial2UCAB.Test/Parcial2UCAB.Test/Controllers/PeliculaControllerTest.cs
+++ src/Parcial2UCAB.Test/Parcial2UCAB.Test/Controllers/PeliculaControllerTest.cs
@@ -6,6 +6,8 @@ using Moq;
 using Parcial2UCAB.Controllers;
 using Parcial2UCAB.Persistence.DAOs.Interfaces;
 using Parcial2UCAB.Requests;
+using Parcial2UCAB.Persistence.Entities;
+using Parcial2UCAB.Responses;
 
 namespace Parcial2UCAB.Test.Controllers
 {
@@ -53,5 +55,75 @@ namespace Parcial2UCAB.Test.Controllers
                 Assert.NotNull(ex);
             }
         }
+
+        [Fact]
+        public async Task UpdatePelicula()
+        {
+            _daoMock
+                .Setup(x => x.UpdatePelicula(It.IsAny<PeliculaRequest>()))
+                .ReturnsAsync(new Guid());
+
+            var result = await _controller.UpdatePelicula(new PeliculaRequest());
+
+            Assert.IsType<Guid>(result);
+        }
+
+        [Fact]
+        public async Task ThrowUpdatePelicula()
+        {
+            try
+            {
+                _daoMock
+                    .Setup(x => x.UpdatePelicula(It.IsAny<PeliculaRequest>()))
+                    .ThrowsAsync(new Exception());
+
+                var result = await _controller.UpdatePelicula(new PeliculaRequest());
+            }
+            catch (Exception ex)
+            {
+                Assert.NotNull(ex);
+            }
+        }
+
+        [Fact]
+        public async Task GetPelicula()
+        {
+            var nombrePelicula = "La mascara de pedro";
+            try
+            {
+                _daoMock
+                    .Setup(x => x.GetPeliculaxNombre(It.IsAny<string>()))
+                    .ThrowsAsync(new Exception());
+
+                var result = await _controller.GetPelicula(nombrePelicula);
+                var application = new ApplicationResponse<List<PeliculaResponse>>();
+
+                Assert.IsType(application.GetType(), result.GetType());
+            }
+            catch (Exception ex)
+            {
+                Assert.NotNull(ex);
+            };
+        }
+
+
+        [Fact]
+        public async Task ThrowGetPelicula()
+        {
+            var nombrePelicula = "";
+            try
+            {
+                _daoMock
+                    .Setup(x => x.GetPeliculaxNombre(It.IsAny<string>()))
+                    .ThrowsAsync(new Exception());
+
+                var result = await _controller.GetPelicula(nombrePelicula);
+            }
+            catch (Exception ex)
+            {
+                Assert.NotNull(ex);
+            };
+        }
+
     }
 }
