d673f441047af975c453caf3c6a33697d8b1ae9f
myangui <miguelyg22@gmail.com>
2023-01-20 19:53:33 -0400

    Migracion
    No aparecia el archivo de migracion

---
+++ src/Parcial2UCAB/Migrations/20230120235224_PeliculaFormato.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial2UCAB.Migrations
+{
+    public partial class PeliculaFormato : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Formato",
+                table: "Peliculas",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Formato",
+                table: "Peliculas");
+        }
+    }
+}
