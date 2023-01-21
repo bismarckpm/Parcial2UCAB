dc133a91106934c574c878f0a26c713cf059155a
Samuel Luis <sjluis.18@est.ucab.edu.ve>
2023-01-20 20:39:16 -0400

    Pregunta 3: Agregue el campo Formato a Pelicula

---
+++ src/Parcial2UCAB/Migrations/20230121001852_AgregarFormatoAPelicula.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial2UCAB.Migrations
+{
+    public partial class AgregarFormatoAPelicula : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "formato",
+                table: "Peliculas",
+                type: "integer",
+                nullable: true,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "formato",
+                table: "Peliculas");
+        }
+    }
+}
--- src/Parcial2UCAB/Persistence/Entities/Pelicula.cs
+++ src/Parcial2UCAB/Persistence/Entities/Pelicula.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Parcial2UCAB.Persistence.Entities
 {
+    public enum FormatoPelicula
+    {
+        [EnumMember(Value = "2D")]
+        DosD,
+        [EnumMember(Value = "3D")]
+        TresD,
+        [EnumMember(Value = "4DX")]
+        CuatroDX,
+    }
     public class Pelicula : BaseEntity
     {
         public string Titulo { get; set; }
         public bool EnCartelera { get; set; }
+        public FormatoPelicula formato { get; set; }
         public DateTime FechaEstreno { get; set; }
         public ICollection<Genero> Generos { get; set; }
         public ICollection<PeliculaActor> PeliculasActores { get; set; }
