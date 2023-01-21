34b4763f90582e05fb2ffc012679f109e449885e
Samuel Luis <sjluis.18@est.ucab.edu.ve>
2023-01-20 21:45:29 -0400

    Pregunta 4: Refactorizacion de Ajustar en la clase Util. PD: En la explicacion de la pregunta 4 respondida en el parcial se menciono que el metodo Ajustar quitaba todos los espacios despues de un salto de linea, cuando esta informacion es falsa, solo quita los espacios posteriores al ultimo salto de linea. Ya el codigo fue arreglado teniendo en cuenta este fallo.

--- src/Parcial2UCAB/Utilities/Util.cs
+++ src/Parcial2UCAB/Utilities/Util.cs
@@ -1,4 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace Parcial2UCAB.Utilities
+{
+    public static class Util
+    {
+        public static bool IsValidEmail(string email)
+        {
+            var regex = new Regex(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+            return regex.IsMatch(email.Trim());
+        }
+
+        public static string Ajustar(string palabra)
+        {
+            if (EsSaltoLinea(palabra)) return palabra;
+
+            if (EsNullOVacio(palabra)) return string.Empty;
+
+            var palabraajustada = BorrarEspaciosIncioLinea(palabra);
+            palabraajustada = palabra.Contains("\n") ? palabra : palabra.Insert(palabraajustada.Length, "\n");
+
+            return palabraajustada;
+        }
+
+        private static string BorrarEspaciosIncioLinea(string palabraajustada)
+        {
+            var lineas = palabraajustada.Split("\n").ToList();
+            var palabraAjustada = lineas[0];
+            //lineas.RemoveAt(0);
+            for (var i= 1; i<lineas.Count; i++)
+            {
+                palabraAjustada += (i < lineas.Count - 1) ? "\n" + lineas[i] : "\n" + lineas[i].TrimStart();
+            }
+            return palabraAjustada;
+        }
+
+        private static bool EsSaltoLinea(string palabra)
+        {
+            return palabra == "\n";
+        }
+
+        private static bool EsNullOVacio(string palabra)
+        {
+            return string.IsNullOrWhiteSpace(palabra);
+        }
+    }
+}
+
+
+/*using System;
 using System.Globalization;
 using System.Text.RegularExpressions;
 
@@ -86,4 +140,4 @@ namespace Parcial2UCAB.Utilities
             return char.IsWhiteSpace(wrd) && (wrd == '\n');
         }
     }
-}
+}*/
